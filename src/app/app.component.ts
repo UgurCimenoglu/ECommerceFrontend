f474f37584abe4b914968f12188017ca967f0cc5
UgurCimenoglu <ugurcimenoglu@hotmail.com>
2022-05-27 02:41:37 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -15,3 +16,7 @@ export class AppComponent {
 
   }
 }
+
+$.get("https://localhost:7045/api/Products", data => {
+  console.log(data);
+})
